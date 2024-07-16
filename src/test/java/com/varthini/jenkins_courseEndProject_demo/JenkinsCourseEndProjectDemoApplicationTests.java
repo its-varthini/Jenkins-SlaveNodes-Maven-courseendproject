b5ad7fd1375de86bfae237a909fb8e552e0d5e40
its-varthini <162994868+its-varthini@users.noreply.github.com>
2024-07-16 12:12:56 +0100

    Add files via upload

---
+++ src/test/java/com/varthini/jenkins_courseEndProject_demo/JenkinsCourseEndProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varthini.jenkins_courseEndProject_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCourseEndProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
